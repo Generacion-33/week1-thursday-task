c9cd971708fef43096985c3df4b2a425ca5fd306
Gabriel <martinezgabrielalejandro4@gmail.com>
2023-08-31 15:59:35 -0300

    task

---
+++ functions.js
@@ -0,0 +1,136 @@
+/******************************/
+/* Ejercicios con funciones: */
+/*****************************/
+
+//------------------------- Exercise1 ✏️-------------------------//
+/**
+ * Exercise1 - La funcion "greet", imprime un mensaje saludando a una persona con su nombre y edad:
+ * `Hola, mi nombre es ${name} y tengo ${age} años`.
+ * Tu labor es completar la funcion para cumplir con la consigna. 
+ * Recuerda que debe imprimir el mensaje ✅.
+ * NO retornar ❌.
+ */
+
+// ----------------------- code here 👇 -----------------------//
+function greet(name, age) {
+  // 👉 resolve aqui
+}
+
+
+//------------------------- Exercise2 ✏️-------------------------//
+/**
+ * Exercise2 - La funcion "welcome", saluda desde el pais que procedes.
+ * Declara una variable global llamada saludo y colocale como valor "Hola los saludo desde:"
+ * Completa la función llamda "welcome". 
+ * Crea una varaible local llamada país y colócale como valor el país de donde vienes.
+ * Tu función "welcome", debe mostrar por consola: "Hola los saludo desde ${pais}".
+ * Recuerda, debes imprimir el mensaje ✅.
+ * NO retornar ❌.
+ */
+
+// ----------------------- code here 👇 -----------------------//
+
+// 👉 resolve aqui
+
+function welcome() {
+  // 👉 resolve aqui
+}
+
+
+//------------------------- Exercise3 ✏️-------------------------//
+/**
+ * Exercise3 - Completa la función "add", que reciba como parámetro numero1 y numero2 y que retorne el resultado de la suma de ambos.
+ * Recueda,debes retornar el resultado ✅.
+ * No imprimir ❌.
+ */
+
+// ----------------------- code here 👇 -----------------------//
+
+function add(numero1, numero2) {
+  // 👉 resolve aqui
+}
+
+
+//------------------------- Exercise4 ✏️-------------------------//
+/**
+ * Exercise4 - Completa la función "double" que reciba como parámetro un numero y debe retornar el doble del numero recibido.
+ * Recuerda, debes retornar el resultado ✅.
+ * No imprimir ❌.
+ */
+
+// ----------------------- code here 👇 -----------------------//
+function double(number) {
+  // 👉 resolve aqui
+}
+
+
+//------------------------- Exercise5 ✏️-------------------------//
+/**
+ * Exercise5 - Completa la funcion "welcome2", para que retorne un mensaje de bienvenida,
+ * Recibe como parámetro el nombre de la persona.
+ * Si no se recibe ningún parámetro debe retornar "Bienvenido Anónimo".
+ * Recuerda, debes retornar el resultado ✅.
+ * No imprimir ❌.
+ */
+
+// ----------------------- code here 👇 -----------------------//
+function welcome2(name = "Anónimo") {
+  // 👉 resolve aqui
+}
+
+
+//------------------------- Exercise6 ✏️-------------------------//
+/**
+ * Exercise6 - Completa la funcion "isVerified".
+ * Comproba si ambos numeros son iguales. Si lo son, retorna true, si no, retorna false.
+ * Recuerda,debes retornar el resultado ✅.
+ * No imprimir ❌. 
+ */
+
+// ----------------------- code here 👇 -----------------------//
+function isVerified(num1, num2) {
+  // 👉 resolve aqui
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------- the end -------------------------//
+module.exports = {
+  greet,
+  welcome,
+  add,
+  double,
+  welcome2,
+  isVerified
+}
